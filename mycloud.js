90961f68293372347b626c1cb3aecee1a8a1a158
Lavadude82 <irvinduarte2009@gmail.com>
2023-07-31 20:39:22 -0500

    added login abbility

--- mycloud.js
+++ mycloud.js
@@ -3,6 +3,7 @@ const fs = require("fs");
 const app = express();
 const { config } = require("./server/config");
 const views = __dirname + "/server/views"
+app.use(express.json())
 app.get("*", (req, res) => {
     console.log(req.path)
   if (req.path == "/") {
@@ -12,8 +13,23 @@ app.get("*", (req, res) => {
       res.redirect("/home");
     }
   }
+  if( req.path == "/favicon"){
+    res.sendFile(__dirname + "/public/favicon.png")
+  }
+  if( req.path == "/icon"){
+    res.sendFile(__dirname + "/public/icon.png")
+  }
+  if(req.path == "/home"){
+    res.sendFile(views + "/Home/index.html")
+  }
+  res.sendFile(views + "/404/index.html")
 });
+app.post("/api/login",(req,res)=>{
+let pw = atob(req.body.pw)
 
+  res.json({correct:pw == config.password})
+
+})
 app.listen(config.port, () => {
   console.log(`Started MyCloud on ${config.port}`);
 });
--- server/config.js
+++ server/config.js
@@ -5,5 +5,6 @@ const config = {
     //Password - set null for none
     password:"Test123",
 
+
 }
 module.exports = {config}
